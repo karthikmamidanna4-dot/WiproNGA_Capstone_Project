38255807f6cbeaa37bf01027f45e6c17dbde4f81
karthikmamidanna4-dot <karthikmamidanna4@gmail.com>
2026-03-26 17:04:53 +0530

    Commit with updated ppt

--- Backend/CustomerSupportAPI/CustomerSupportAPI/obj/Debug/net8.0/CustomerSupportAPI.AssemblyInfo.cs
+++ Backend/CustomerSupportAPI/CustomerSupportAPI/obj/Debug/net8.0/CustomerSupportAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CustomerSupportAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a88a282f6e37a656bbef321cb1834e65ef3d0221")]
 [assembly: System.Reflection.AssemblyProductAttribute("CustomerSupportAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CustomerSupportAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
